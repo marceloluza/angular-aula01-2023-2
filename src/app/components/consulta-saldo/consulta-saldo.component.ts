9ac33f65f4a32671dc8555811aba1bdb767b4d3f
Marcelo Luza <marcelo@siliconvillage.dev>
2024-10-12 11:48:27 -0300

    ajuste ano projeto

---
+++ src/app/components/consulta-saldo/consulta-saldo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-saldo',
+  standalone: true,
+  imports: [],
+  templateUrl: './consulta-saldo.component.html',
+  styleUrl: './consulta-saldo.component.css'
+})
+export class ConsultaSaldoComponent {
+
+}
